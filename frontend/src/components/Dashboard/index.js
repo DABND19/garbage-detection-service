38f058c26f444c79f418eb8d388ca76741ca3bbe
DABND <grigorenkodavid@gmail.com>
2021-12-02 01:29:16 +0300

    Add deploy config.

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -11,6 +11,12 @@ const Container = styled.section`
   grid-template-rows: auto;
   grid-template-columns: auto 600px;
   grid-gap: 1em;
+
+  @media(max-width: 900px) {
+    grid-template-areas: "map" "stats";
+    grid-template-rows: calc(100vh - 2em - 75px) 200px;
+    grid-template-columns: auto;
+  }
 `;
 
 const Stats = styled.div`
