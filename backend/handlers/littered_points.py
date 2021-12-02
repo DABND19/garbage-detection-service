a5f5dd7f697816054cf3d8a0cb6e96e7bb3bbd97
pigerloafer <kruj1998@gmail.com>
2021-12-02 16:34:27 +0300

    add import datetime

--- backend/handlers/littered_points.py
+++ backend/handlers/littered_points.py
@@ -2,18 +2,13 @@ from typing import Tuple
 
 import sqlalchemy
 
+from datetime import datetime
 from db import Session, Selector, Insertor
 from db.models import Camera
 from sqlalchemy import insert
 from .router import router
 from models import SingleCameraInfo
 
-<<<<<<< Updated upstream
-def serialize_point(point: Tuple[Camera, int]):
-    camera, garbage_index = point
-    return {'garbageIndex': garbage_index,
-            'location': [camera.coordinate_x, camera.coordinate_y]}
-=======
 
 def serialize_point(point: Tuple[Camera, int, datetime]):
     camera, garbage_index, time_ = point
@@ -21,7 +16,6 @@ def serialize_point(point: Tuple[Camera, int, datetime]):
             'garbageIndex': garbage_index,
             'time': time_,
             'location': [camera.lat, camera.alt]}
->>>>>>> Stashed changes
 
 
 @router.get('/littered-points')
