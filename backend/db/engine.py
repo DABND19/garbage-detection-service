c94b85197d2057c3c6fc67c9e733cdc4efed549a
DABND <grigorenkodavid@gmail.com>
2021-12-02 04:08:43 +0300

    Nginx env variables.

--- backend/db/engine.py
+++ backend/db/engine.py
@@ -3,17 +3,10 @@ from typing import Callable
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from settings import (
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_PORT,
-    DB_USER
-)
+import settings
 
 
-db_url = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_async_engine(db_url)
+engine = create_async_engine(settings.Database.get_url())
 
 Session: Callable[[], AsyncSession] = sessionmaker(bind=engine, 
                                                    class_=AsyncSession, 
