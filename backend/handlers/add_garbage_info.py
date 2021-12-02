3dfc7e4291246258db5c52f233facdb5fb91dba6
DABND <grigorenkodavid@gmail.com>
2021-12-02 21:48:41 +0300

    MVP #1.

--- backend/handlers/add_garbage_info.py
+++ backend/handlers/add_garbage_info.py
@@ -1,5 +1,9 @@
+from base64 import b64decode
+import os
+from time import time
+from typing import Literal
+
 from pydantic import BaseModel
-from sqlalchemy.orm import session
 
 from db import Session
 from db.models import GarbageLog
@@ -7,15 +11,35 @@ from db.models import GarbageLog
 from .router import router
 
 
+class Photo(BaseModel):
+    extension: Literal['jpeg', 'png', 'jpg']
+    data: str
+
+
 class SingleCameraInfo(BaseModel):
     cameraId: int
     garbageIndex: int
+    photo: Photo
+
+
+def save_photo(info: SingleCameraInfo) -> str:
+    file_name = f'{info.cameraId}_{int(time())}.{info.photo.extension}'
+    file_path = os.path.join('photos/', file_name)
+
+    raw_data = b64decode(info.photo.data)
+    with open(file_path, 'wb') as f:
+        f.write(raw_data)
+
+    return file_path
 
 
 @router.post('/add-garbage-info')
 async def handler(info: SingleCameraInfo):
     async with Session() as db_session:
         obj = GarbageLog(camera_id=info.cameraId, 
-                         garbage_index=info.garbageIndex)
+                         garbage_index=info.garbageIndex, 
+                         photo_path=save_photo(info))
         db_session.add(obj)
         await db_session.commit()
+    
+    return {'photo': obj.photo_path}
--- frontend/src/components/Dashboard/GarbageMap.js
+++ frontend/src/components/Dashboard/GarbageMap.js
@@ -3,31 +3,13 @@ import { Map, YMaps, ObjectManager } from "react-yandex-maps";
 import { useQuery } from 'react-query';
 import { fetchLitteredPoints } from '../../api';
 
-const featuresExample = [
-  {
-    type: "Feature",
-    id: 0,
-    geometry: { type: "Point", coordinates: [55.831903, 37.411961] },
-    properties: {
-      balloonContentHeader:
-        "<font size=3><b><a target='_blank' href='https://yandex.ru'>Здесь может быть ваша ссылка</a></b></font>",
-      balloonContentBody:
-        "<p>Ваше имя: <input name='login'></p><p><em>Телефон в формате 2xxx-xxx:</em>  <input></p><p><input type='submit' value='Отправить'></p>",
-      balloonContentFooter:
-        "<font size=1>Информация предоставлена: </font> <strong>этим балуном</strong>",
-      clusterCaption: "<strong><s>Еще</s> одна</strong> метка",
-      hintContent: "<strong>Текст  <s>подсказки</s></strong>",
-    },
-  },
-]
-
 const GarbageMap = () => {
   const { data } = useQuery('littered-points', fetchLitteredPoints)
 
   return (
     <YMaps>
       <Map
-        defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+        defaultState={{ center: [55.786778667350575, 49.12538845509848], zoom: 12 }}
         width="100%"
         height="100%"
       >
@@ -46,14 +28,8 @@ const GarbageMap = () => {
             id: point.cameraId,
             geometry: { type: "Point", coordinates: point.location },
             properties: {
-              balloonContentHeader:
-                "<font size=3><b><a target='_blank' href='https://yandex.ru'>Здесь может быть ваша ссылка</a></b></font>",
-              balloonContentBody:
-                "<p>Ваше имя: <input name='login'></p><p><em>Телефон в формате 2xxx-xxx:</em>  <input></p><p><input type='submit' value='Отправить'></p>",
-              balloonContentFooter:
-                "<font size=1>Информация предоставлена: </font> <strong>этим балуном</strong>",
-              clusterCaption: "<strong><s>Еще</s> одна</strong> метка",
-              hintContent: "<strong>Текст  <s>подсказки</s></strong>",
+              hintContent: "Индекс загрязненности: 7",
+              balloonContentBody: "<img src='https://greenstroyspb.ru/ckfinder/userfiles/images/trash(1).jpg' width='300px'></img>",
             },
           }))}
           modules={[
