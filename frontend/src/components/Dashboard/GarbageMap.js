872ba338dd03d22b9a19d61d8fbab2f8caeb47ce
DABND <grigorenkodavid@gmail.com>
2021-12-03 20:51:46 +0300

    Frontend deploy.

--- frontend/src/components/Dashboard/GarbageMap.js
+++ frontend/src/components/Dashboard/GarbageMap.js
@@ -4,7 +4,9 @@ import { useQuery } from "react-query";
 import { fetchLitteredPoints } from "../../api";
 
 const GarbageMap = () => {
-  const { data } = useQuery("littered-points", fetchLitteredPoints);
+  const { data } = useQuery("littered-points", fetchLitteredPoints, {
+    refetchInterval: 15000
+  });
 
   return (
     <YMaps>
